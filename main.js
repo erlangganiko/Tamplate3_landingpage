af62332f7befcd427a930fa89f4aae8e7f357d99
psyzxxy <112676871+psyzxxy@users.noreply.github.com>
2025-06-09 03:43:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -8,11 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
   getBtn.addEventListener('click', (e) => {
     e.preventDefault();
     links.innerHTML = `
-      <a href="https://wa.me/${whatsapp}" target="_blank">📱 WhatsApp</a>
-      <a href="mailto:info@example.com">✉️ Email</a>
-      <a href="https://instagram.com/moemtaz" target="_blank">📸 IG</a>
-      <a href="https://tiktok.com/@moemtaz" target="_blank">🎵 TikTok</a>
-    `;
+  <a href="https://wa.me/${whatsapp}" target="_blank" class="icon-link wa">WhatsApp</a>
+  <a href="mailto:info@example.com" class="icon-link email">Email</a>
+  <a href="https://instagram.com/moemtaz" target="_blank" class="icon-link ig">Instagram</a>
+  <a href="https://tiktok.com/@moemtaz" target="_blank" class="icon-link tiktok">TikTok</a>
+`;
+
     popup.classList.remove('hidden');
   });
 
